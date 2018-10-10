46d8695872a972d19398a28723d24ae10d7aebfe
seasoftmayzinmarwin <win.may.zin.mar@seasoft.asia>
2018-10-10 14:07:20 +0630

    Add Home page

--- index.php
+++ index.php
@@ -1,122 +1,186 @@
 <?php include('layout/header.php') ?>
 <?php include('layout/sidebar.php') ?>
-  <div class="content">
+<div class="content">
 
-    <div class="card h-card h-p-30">
-      <img src="image/smartphone-release.png" width="100%">
-    </div>
-
-    <div class="space"></div>
-
-    <div class="card h-card">
-      <div class="card-header h-border-buttom">
-        <div class="d-flex justify-content-between">
-          <div class="col-md-4">
-            <span><strong>おすすめの求職者</strong></span>
-          </div>
-          <div class="col-md-7">
-            <span class="text-center">気になる求職者にスカウトを送って、応募を促しましょう</span>
-          </div>
-            
-            <div class="col-md-1">
-              <button class="btn h-button-sm">Button</button>
-            </div>
-          
-        </div>
-      </div><!-- .card-header -->
-      <div class="card-body h-no-padding">
-        <div class="d-flex h-flex recommended-candidates">
-          <div class="p-2 flex-fill e-flex-fill">
-              <a class="nurse-block nurse-small-short text-center space-pd-20" href="">
-              <div class="card-font-black">看護師/准看護師</div>
-              <div class="space-mt-10">
-                <span class="nurse-label space-mt-auto  label-semicircle">
-                  <div class="nurse-group-item">
-                    <div class="nurse-group-item-box">
-                      <span class="card-font-md">57歳</span>
-                    </div>
-                    <div class="nurse-group-item-box">
-                      <span class="">女性</span>
-                    </div>
-                  </div>
-                </span>
-              </div>
-
-              <div class="card-font-gray">神奈川県横浜市戸塚区</div>
-              <div class="card-font-gray">会員番号 : 334974</div>
-              </a>
-          </div>
-          <div class="p-2 flex-fill e-flex-fill">
-            <a class="nurse-block nurse-small-short text-center space-pd-20" href="">
-              <div class="card-font-black">看護師/准看護師 (7年未満) 助産師 (10年以上)</div>
-              <div class="space-mt-10">
-                <span class="nurse-label space-mt-auto  label-semicircle">
-                  <div class="nurse-group-item">
-                    <div class="nurse-group-item-box">
-                      <span class="card-font-md">49歳</span>
-                    </div>
-                    <div class="nurse-group-item-box">
-                      <span class="">女性</span>
-                    </div>
-                  </div>
-                </span>
-              </div>
-
-              <div class="card-font-gray">神奈川県横浜市戸塚区</div>
-              <div class="card-font-gray">会員番号 : 334974</div>
-              </a>
-          </div>
-          <div class="p-2 flex-fill e-flex-fill">
-            <a class="nurse-block nurse-small-short text-center space-pd-20" href="">
-              <div class="card-font-black">看護師/准看護師 (5年未満)</div>
-              <div class="space-mt-10">
-                <span class="nurse-label space-mt-auto  label-semicircle">
-                  <div class="nurse-group-item">
-                    <div class="nurse-group-item-box">
-                      <span class="card-font-md">40歳</span>
-                    </div>
-                    <div class="nurse-group-item-box">
-                      <span class="">女性</span>
-                    </div>
-                  </div>
-                </span>
-              </div>
-
-              <div class="card-font-gray">神奈川県横浜市戸塚区</div>
-              <div class="card-font-gray">会員番号 : 334974</div>
-              </a>
-          </div>
-        </div>
-      </div>
-    </div><!-- .card-head -->
-
-    <div class="space"></div>
-
-    <div class="row h-no-margin application">
-      <div class="col-md-8 h-no-p-l h-mobile-no-p-r">
-        <div class="card h-card d-flex">
-          <div class="row h-no-margin">
-            <div class="p-2 flex-fill h-border-right">
-              EE
-            </div>
-            <div class="p-2 flex-fill">
-              EE
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-4 h-no-padding">
-        <div class="card h-card d-flex">
-          <div class="row h-no-margin">
-            <div class="p-2 flex-fill h-border-right">
-              EE
-            </div>
-            <div class="p-2 flex-fill">
-              EE
-            </div>
-          </div>
-        </div>
-      </div>
+  <div class="card h-card h-p-30">
+    <img src="image/smartphone-release.png" width="100%">
   </div>
+
+  <div class="space"></div>
+
+  <div class="card h-card">
+    <div class="card-header h-border-buttom">
+      <div class="d-flex justify-content-between">
+        <div class="col-md-4">
+          <span><strong>おすすめの求職者</strong></span>
+        </div>
+        <div class="col-md-6">
+          <span class="text-center">気になる求職者にスカウトを送って、応募を促しましょう</span>
+        </div>
+
+        <div class="col-md-2">
+          <button class="btn h-button-sm">もっとみる</button>
+        </div>
+
+      </div>
+    </div><!-- .card-header -->
+    <div class="card-body h-no-padding">
+      <div class="d-flex h-flex recommended-candidates">
+        <div class="p-2 flex-fill e-flex-fill">
+          <a class="card-block card-small-short text-center space-pd-20" href="">
+            <div class="card-font-black">看護師/准看護師</div>
+            <div class="space-mt-10">
+              <span class="card-label space-mt-auto  label-semicircle">
+                <div class="card-group-item">
+                  <div class="card-group-item-box">
+                    <span class="card-font-md">57歳</span>
+                  </div>
+                  <div class="card-group-item-box">
+                    <span class="">女性</span>
+                  </div>
+                </div>
+              </span>
+            </div>
+
+            <div class="card-font-grey">神奈川県横浜市戸塚区</div>
+            <div class="card-font-grey">会員番号 : 334974</div>
+          </a>
+        </div>
+        <div class="p-2 flex-fill e-flex-fill">
+          <a class="card-block card-small-short text-center space-pd-20" href="">
+            <div class="card-font-black card-font-ellipsis">看護師/准看護師 (7年未満) 助産師 (10年以上)</div>
+            <div class="space-mt-10">
+              <span class="card-label space-mt-auto  label-semicircle">
+                <div class="card-group-item">
+                  <div class="card-group-item-box">
+                    <span class="card-font-md">49歳</span>
+                  </div>
+                  <div class="card-group-item-box">
+                    <span class="">女性</span>
+                  </div>
+                </div>
+              </span>
+            </div>
+
+            <div class="card-font-grey">神奈川県横浜市戸塚区</div>
+            <div class="card-font-grey">会員番号 : 334974</div>
+          </a>
+        </div>
+        <div class="p-2 flex-fill e-flex-fill">
+          <a class="card-block card-small-short text-center space-pd-20" href="">
+            <div class="card-font-black">看護師/准看護師 (5年未満)</div>
+            <div class="space-mt-10">
+              <span class="card-label space-mt-auto  label-semicircle">
+                <div class="card-group-item">
+                  <div class="card-group-item-box">
+                    <span class="card-font-md">40歳</span>
+                  </div>
+                  <div class="card-group-item-box">
+                    <span class="">女性</span>
+                  </div>
+                </div>
+              </span>
+            </div>
+
+            <div class="card-font-grey">神奈川県横浜市戸塚区</div>
+            <div class="card-font-grey">会員番号 : 334974</div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div><!-- .card-head -->
+
+  <div class="space"></div>
+
+  <div class="row h-no-margin application">
+    <div class="col-md-8 h-no-p-l h-mobile-no-p-r">
+      <div class="card h-card d-flex">
+        <div class="row h-no-margin">
+          <div class="p-2 e-flex-fill flex-fill h-border-right">
+            <a class="card-block card-small-short text-center space-pd-20" href="">
+              <div class="card-fw-bold">未確認の応募</div>
+              <span class="card-fw-md-bold">0</span><span class="card-font-sm-grey grey-pd-sm">件</span>
+            </a>
+          </div>
+          <div class="p-2 e-flex-fill flex-fill">
+           <a class="card-block card-small-short text-center space-pd-20" href="">
+            <div class="card-fw-bold">未読メッセージ</div>
+            <span class="card-fw-md-bold">0</span><span class="card-font-sm-grey grey-pd-sm">件</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-4 h-no-padding">
+    <div class="card h-card d-flex">
+      <div class="row h-no-margin">
+        <div class="p-2 e-flex-fill flex-fill h-border-right">
+          <a class="card-block card-small-short text-center space-pd-20" href="">
+            <div class="card-fw-bold">募集中の施設</div>
+            <span class="card-fw-md-bold">1</span><span class="card-font-sm-grey grey-pd-sm">件</span>
+          </a>
+        </div>
+        <div class="p-2 e-flex-fill flex-fill">
+          <a class="card-block card-small-short text-center space-pd-20" href="">
+            <div class="card-fw-bold">募集中の求人</div>
+            <span class="card-fw-md-bold">7</span><span class="card-font-sm-grey grey-pd-sm">件</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="space"></div>
+<section class="card-list">
+  <div class="row h-no-margin">
+    <div class="clearfix">
+      <div class="card-list-left">
+        <span class="u-fw-bold pull-left">お知らせ</span>
+      </div>
+      <div class="card-list-right pull-right">
+        <div class="c-feed">
+          <ol class="c-feed__inner">
+            <li class="c-feed__item">
+              <div class="c-feed__item-inner">
+                <div class="c-feed__date">2018/09/11</div>
+                <a class="c-link c-feed__link">「公認心理師」の取り扱い開始についてのお知らせ</a>
+              </div>
+            </li>
+            <li class="c-feed__item">
+              <div class="c-feed__item-inner">
+                <div class="c-feed__date">2018/09/03</div>
+                <a class="c-link c-feed__link">スマートフォン向け機能の提供開始のお知らせ</a>
+              </div>
+            </li>
+            <li class="c-feed__item">
+              <div class="c-feed__item-inner">
+                <div class="c-feed__date">2018/07/01</div>
+                <a class="c-link c-feed__link">ジョブメドレー 利用規約改定のお知らせ</a>
+              </div>
+            </li>
+            <li class="c-feed__item">
+              <div class="c-feed__item-inner">
+                <div class="c-feed__date">2018/06/27</div>
+                <a class="c-link c-feed__link">新機能「職場の環境」リリースのお知らせ</a>
+              </div>
+            </li>
+            <li class="c-feed__item">
+              <div class="c-feed__item-inner">
+                <div class="c-feed__date">2018/06/25</div>
+                <a class="c-link c-feed__link">※解消済み※求職者検索画面の不具合について</a>
+              </div>
+            </li>
+            <li class="c-feed__item">
+              <div class="c-feed__item-inner">
+                <div class="c-feed__date">2016/10/04</div>
+                <a class="c-link c-feed__link">取り扱い職種の追加と職種追加に伴う採用単価表更新のお知らせ</a>
+              </div>
+            </li>
+          </ol>
+        </div>
+      </div>
+    </div>
+  </div>
+</section><!-- .card-list -->
 
 <?php include('layout/footer.php') ?>
