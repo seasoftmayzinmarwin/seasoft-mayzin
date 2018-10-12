2895a8962b4dc83f0fcede50a3678296121fd069
seasoftmayzinmarwin <win.may.zin.mar@seasoft.asia>
2018-10-12 10:21:43 +0630

    Add Register 3

---
+++ register3.php
@@ -0,0 +1,59 @@
+<?php include('layout/login-header.php'); ?>
+<div class="space"></div>
+<div class="register-block">
+	<main class="main-block">
+		<div>
+			<div class="main-form-auto">
+				<div class="main-content">
+					<div class="main-content-inner">
+						<div class="main-segment">
+							<div class="main-content-detail">
+								<div class="main-register-head"><p>求人掲載のお申し込み</p></div>
+								<div class="space"></div>
+								<div class="main-content-register">
+									<div class="main-form-guide text-center">
+										<div class="space"></div>
+										<p class="card-fw-bold">ジョブメドレーは採用するまで無料で利用できます！<br>掲載料0円、原稿作成料0円、応募課金0円、採用時の単価は25,000円~となります。<br>採用単価は経験年数や年収で左右されることはありません！</p>
+										<p class="card-fw-bold">ご利用の流れは以下をご覧ください。</p>
+									</div>
+									<div class="space"></div>
+									<div class="card-flowchart">
+										<div class="card-flowchart-block clearfix">
+											<div class="card-flowchart-content ">
+												<div class="card-flowchart-head">1.</div>
+												<div class="card-flowchart-detail e-detail-text">お申し込みフォームに<br>必要事項を記入</div>
+											</div><!-- .card-flowchart-mai -->
+											<div class="card-flowchart-arrow"></div>
+										</div><!-- .card-flowchart-block -->
+
+										<div class="card-flowchart-block card-flowchart-m  clearfix">
+											<div class="card-flowchart-content card-flowchart-main">
+												<div class="card-flowchart-head">2.</div>
+												<div class="card-flowchart-detail e-detail-text">ジョブメドレー運営事務局による<br>お申し込み内容の確認・審査<br>(最短当日〜3営業日以内)</div>
+											</div><!-- .card-flowchart-mai -->
+											<div class="card-flowchart-arrow arrow-complete"></div>
+										</div><!-- .card-flowchart-block -->
+
+										<div class="card-flowchart-block card-flowchart-m clearfix">
+											<div class="card-flowchart-content ">
+												<div class="card-flowchart-head">3.</div>
+												<div class="card-flowchart-detail e-detail-text">お申し込みフォームに<br>必要事項を記入</div>
+											</div><!-- .card-flowchart-mai -->
+											<div class="card-flowchart-arrow"></div>
+										</div><!-- .card-flowchart-block -->
+									</div><!-- .card-flowchart -->
+									<div class="space"></div>
+									<div class="confirm-page">
+										p
+										
+									</div>
+									</div><!-- .main-content-register -->
+								</div><!-- .main-content-detail -->
+							</div><!-- .main-segment -->
+						</div><!-- .main-content-inner -->
+					</div><!-- .main-content -->
+				</div><!-- .main-form-w -->
+			</div>
+
+		</main><!-- .register-main-block -->
+ </div><!-- .register-block -->
